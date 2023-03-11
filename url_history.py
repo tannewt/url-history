e6729ece2a47fca74ad79eae03244e2bd485244b
Scott Shawcroft <scott@tannewt.org>
2023-03-11 08:43:56 -0800

    Support crawl delay and retries

--- url_history.py
+++ url_history.py
@@ -5,6 +5,21 @@ import sqlite3
 import lzma
 import hashlib
 import datetime
+import urllib3
+import time
+
+# import logging
+
+# from http.client import HTTPConnection
+# HTTPConnection.debuglevel = 1
+
+# logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
+
+headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/110.0'}
 
 __version__ = "0.0.1"
 
@@ -19,13 +34,29 @@ class HistorySession:
         except sqlite3.OperationalError:
             pass
 
-    def get(self, url, fetch_again=False):
+        self.requests = requests.Session()
+        retry_policy = urllib3.Retry(total=10, backoff_factor=0.1)
+        a = requests.adapters.HTTPAdapter(max_retries=retry_policy)
+        self.requests.mount('https://', a)
+
+    def get(self, url, fetch_again=False, crawl_delay=0, only_after=None):
         now = datetime.datetime.now()
         cur = self.db.cursor()
         cur.execute("SELECT sha256, content_xz, first_fetch, last_fetch FROM pages WHERE url = ? ORDER BY last_fetch DESC LIMIT 1", (url,))
         past_fetch = cur.fetchone()
         if past_fetch is None or fetch_again:
-            response = requests.get(url)
+            if only_after:
+                headers["If-Modified-Since"] = only_after.strftime("%a, %d %b %Y %H:%M:%S GMT")
+            elif "If-Modified-Since" in headers:
+                del headers["If-Modified-Since"]
+            before = time.monotonic()
+            response = self.requests.get(url, headers=headers, timeout=30)
+            after = time.monotonic()
+            diff = after - before
+            if diff > 1:
+                print("Slow request:", diff)
+            if response.status_code == 304:
+                return None
             content = response.content
             sha256 = hashlib.sha256(content).digest()
             if past_fetch and sha256 == past_fetch[0]:
@@ -34,6 +65,7 @@ class HistorySession:
                 content_xz = lzma.compress(content)
                 cur.execute("INSERT INTO pages (url, sha256, content_xz, first_fetch, last_fetch) VALUES (?, ?, ?, ?, ?)", (url, sha256, content_xz, now, now))
             self.db.commit()
+            time.sleep(crawl_delay)
             return content
         return lzma.decompress(past_fetch[1])
 
